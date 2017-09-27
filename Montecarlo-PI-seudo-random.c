694bf2e026d7f9fe9c02d543a95383685010a8c9
ayayitox <ayayitox@AyayitoxPC>
2017-09-27 00:53:54 -0500

    Actualizado

--- Montecarlo-PI-seudo-random.c
+++ Montecarlo-PI-seudo-random.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-#include <mpi.h>
 #include <math.h>
-#include <string.h>
-#include <stdlib.h>
 
 
 static long MULTIPLER = 1366;
@@ -10,22 +7,9 @@ static long ADDEND = 150889;
 static long PMOD = 714025;
 
 
-double random(){
-	long random_next;
-	double ret_val;
-
-	// calculando un numero aleatorio entero desde cero a mod
-
-	random_next = (MULTIPLER * random_last+ ADDEND) % PMOD;
-	random_last = random_next;
-
-	//shit into preset range
-
-	ret_val = ((double)random_next/(double)PMOD) * (random_hi-random_low) + random_low;   
-	return ret_val;	
-}
-
-
+double random_last;
+double random_low; 
+double random_hi;
 
 void seed(double low_in, double hi_in ){
 
@@ -46,21 +30,45 @@ void seed(double low_in, double hi_in ){
 } 
 
 
+double random(){
+	long random_next;
+	double ret_val;
 
-int main(int argc, char *argv[]) {
+	// calculando un numero aleatorio entero desde cero a mod
 
+	random_next = fmod( (MULTIPLER * random_last+ ADDEND) , PMOD);
+	random_last = random_next;
 
-
-
-
-
-
-
-
-
+	//Cambia el rango predeterminado
 
 
+	ret_val = ((double)random_next/(double)PMOD) * (random_hi-random_low) + random_low;   
+	return ret_val;	
+}
 
+
+
+
+
+
+int main() {
+	int a;
+	double aleatorio;
+	printf("Esto es una prubea de aleatorios \n");
+
+	printf("Ingrese una opcion diferente de cero \n");
+	scanf("%d",&a);
+	seed(100,10000);
+
+	aleatorio=random();
+	printf("El last %lf\n",random_last);
+	printf("%lf \n",aleatorio);
+
+	/*while(a!=0){
+		printf("%lf \n",random);
+		printf("Ingrese una opcion diferente de cero \n");
+		scanf("%d",a);
+	}*/
 
 
 	return(0);
